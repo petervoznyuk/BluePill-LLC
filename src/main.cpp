4d77309097973fe5dfa0c85565b849489ca75b07
Andrew Kassab <andrewkassab17@gmail.com>
2023-01-20 20:40:26 -0800

    Added untested trajectory generation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <cmath>
+#include <ArduinoEigen.h>
 
 #define ENC_CHIP_SELECT_LEFT PA4
 #define ENC_CHIP_SELECT_RIGHT PB5
@@ -33,9 +34,11 @@ float right_previous_error = 0;
 double previous_time;
 float kp = 1;
 float ki = 0;
-float kd = 0;
+float kd = 0.0006;
 float max_pwm = 40;
 
+array<float,6> cx;
+array<float,6> cy;
 
 /*
 Read left and right motor angles from the encoders.
@@ -207,6 +210,47 @@ void command_motors(float x_pos, float y_pos, double current_time, double previo
     set_motor_pwms(left_pwm, right_pwm);
 }
 
+
+array<float,3> get_intermediate_point(float x, float y, float vx, float vy, float final_time, float straight_length) {
+    float final_velocity_magnitude = sqrt(pow(vx,2) + pow(vy,2));
+    float reverse_x_component = -vx / final_velocity_magnitude * straight_length;
+    float reverse_y_component = -vy / final_velocity_magnitude * straight_length;
+    
+    float intermediate_x = x + reverse_x_component;
+    float intermediate_y = y + reverse_y_component;
+
+    float intermediate_t = final_time - straight_length / final_velocity_magnitude;
+
+    return {{intermediate_x, intermediate_y, intermediate_t}};
+}
+
+array<float,12> get_trajectory_coeffs(float t0, float x0, float y0, float vx0, float vy0, float tf, float xf, float yf, float vxf, float vyf, float d) {
+    array<float,3> intermediate_params = get_intermediate_point(xf, yf, vxf, vyf, tf, 0.1);
+
+    float x1 = intermediate_params[0];
+    float y1 = intermediate_params[1];
+    float t1 = intermediate_params[2];
+    
+    Eigen::Matrix<float,6,6> mat;
+
+    mat << 1, t0, pow(t0,2), pow(t0,3), pow(t0,4), pow(t0,5),
+        0, 1, 2*t0, 3*pow(t0,2), 4*pow(t0,3), 5*pow(t0,4),
+        1, t1, pow(t1,2), pow(t1,3), pow(t1,4), pow(t1,5),
+        0, 1, 2*t1, 3*pow(t1,2), 4*pow(t1,3), 5*pow(t1,4),
+        1, tf, pow(tf,2), pow(tf,3), pow(tf,4), pow(tf,5),
+        0, 1, 2*tf, 3*pow(tf,2), 4*pow(tf,3), 5*pow(tf,4);
+
+    Eigen::Vector<float,6> bx(x0, vx0, x1, vxf, xf, vxf);
+
+    Eigen::Vector<float,6> ax = mat.colPivHouseholderQr().solve(bx);
+
+    Eigen::Vector<float,6> by(y0, vy0, y1, vyf, yf, vyf);
+
+    Eigen::Vector<float,6> ay = mat.colPivHouseholderQr().solve(by);
+
+    return {{ax[0], ax[1], ax[2], ax[3], ax[4], ax[5], ay[0], ay[1], ay[2], ay[3], ay[4], ay[5]}};
+}
+
 void setup() {
     Serial.begin(460800);
     SPI.beginTransaction(SPISettings(460800, MSBFIRST, SPI_MODE1));
@@ -228,9 +272,27 @@ void setup() {
     digitalWrite(LEFT_MOTOR_DIR_PIN, LOW);
     digitalWrite(RIGHT_MOTOR_DIR_PIN, LOW);
 
-    // home_table(7, 6, 10);
-    left_revolutions = 0;
-    right_revolutions = 0;
+    home_table(7, 6, 10);
+
+    // Trajectory parameters
+    float t0 = 0;
+    array<float,2> start_angles = read_motor_angles();
+    array<float,2> start_position = theta_to_xy(start_angles[0], start_angles[1]);
+    float x0 = start_position[0];
+    float y0 = start_position[1];
+    float vx0 = 0;
+    float vy0 = 0;
+
+    float tf = 0.75;
+    float xf = 0.5;
+    float yf = 0.5;
+    float vxf = 0;
+    float vyf = 0.5;
+
+    array<float,12> coeffs = get_trajectory_coeffs(t0, x0, y0, vx0, vy0, tf, xf, yf, vxf, vyf, 0.1);
+
+    cx = {{coeffs[0], coeffs[1], coeffs[2], coeffs[3], coeffs[4], coeffs[5]}};
+    cy = {{coeffs[6], coeffs[7], coeffs[8], coeffs[9], coeffs[10], coeffs[11]}};
 
     delay(500);
 
@@ -246,12 +308,23 @@ void setup() {
 }
 
 void loop() {
-    double current_time = (micros() - start_time) / 1000000.0;
+    double t = (micros() - start_time) / 1000000.0;
 
-    float x_pos = circle_radius * (cos(angular_frequency * current_time)-1);
-    float y_pos = circle_radius * sin(angular_frequency * current_time);
+    // float x_pos = circle_radius * (cos(angular_frequency * current_time)-1);
+    // float y_pos = circle_radius * sin(angular_frequency * current_time);
 
-    command_motors(x_pos, y_pos, current_time, previous_time);
+    float x_pos = cx[0] + cx[1]*t + cx[2]*pow(t,2) + cx[3]*pow(t,3) + cx[4]*pow(t,4) + cx[5]*pow(t,5);
+    float y_pos = cy[0] + cy[1]*t + cy[2]*pow(t,2) + cy[3]*pow(t,3) + cy[4]*pow(t,4) + cy[5]*pow(t,5);
 
-    previous_time = current_time;
+    Serial.print("Time (s): ");
+    Serial.print(t);
+    Serial.print("\tX (cm): ");
+    Serial.print(x_pos*100);
+    Serial.print("\tY (cm): ");
+    Serial.println(y_pos*100);
+    // command_motors(x_pos, y_pos, t, previous_time);
+
+    previous_time = t;
+
+    delay(1000);
 }
