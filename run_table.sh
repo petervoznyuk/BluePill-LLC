4f1a52e29f2967b193737d9a0278147fb75ec562
Andrew Kassab <andrewkassab17@gmail.com>
2023-04-07 00:57:44 -0700

    Reading serial on BluePill

--- run_table.sh
+++ run_table.sh
@@ -1,1 +1,5 @@
-~/.platformio/penv/Scripts/platformio.exe run --target upload --target monitor --environment bluepill_f103c8 --monitor-port COM9 > data/$(date +%Y%m%d)_time$(date +%H%M).csv
+# Compile code and upload it to the Blue Pill
+~/.platformio/penv/Scripts/platformio.exe run --target upload --environment bluepill_f103c8
+# Start serial monitor to read from and write to the Blue Pill
+# See https://docs.platformio.org/en/latest/core/userguide/device/cmd_monitor.html#cmd-device-monitor-filters
+~/.platformio/penv/Scripts/platformio.exe device monitor --eol=LF --filter send_on_enter > data/serialtest_$(date +%Y%m%d)_time$(date +%H%M).csv
