72b2c5157b98db051ac10c37951457f069cb5e03
Joe Thurston <55476354+joethrstn@users.noreply.github.com>
2024-04-01 15:56:09 -0700

    Added in Agent Code and began cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,8 @@
 #include <SPI.h>
 #include <cmath>
 #include <ArduinoEigen.h>
-// #include "ArduinoEigen/ArduinoEigenCommon.h"
+#include <math.h>
 
-// SWITCHED LEFT AND RIGHT
 #define ENC_CHIP_SELECT_LEFT PB7
 #define ENC_CHIP_SELECT_RIGHT PB6
 #define LEFT_MOTOR_PWM_PIN PA_8
@@ -12,27 +11,38 @@
 #define LEFT_MOTOR_DIR_PIN PB15
 #define RIGHT_MOTOR_DIR_PIN PA9
 
-// // PREVIOUS DIRECTIONS
-// #define ENC_CHIP_SELECT_LEFT PB6 // prev PA4
-// #define ENC_CHIP_SELECT_RIGHT PB7 // prev PB5
-// #define LEFT_MOTOR_PWM_PIN PA_8 // PA_10 prev PB_7
-// #define RIGHT_MOTOR_PWM_PIN PA_10 // PA_8 prev PB_8
-// #define LEFT_MOTOR_DIR_PIN PB15 // PA9 prev PB3
-// #define RIGHT_MOTOR_DIR_PIN PA9 // PB15 prev PB4
-
 #define SPI_MISO_PIN PB4
 #define SPI_SCLK_PIN PB3
 #define ENABLE_MOTOR_PIN PB11
-#define DUTY_CYCLE_CONVERSION 1024 // Accepted duty cycle values are 0-1024
-#define PWM_FREQ_HZ 10000
-#define ROLLOVER_ANGLE_DEGS 180
-#define PULLEY_RADIUS 0.035 //meters
-#define X_OFFSET 0.0699 //meters
-#define Y_OFFSET 0.0508 //meters
-#define X_MIN X_OFFSET //meters
-#define X_MAX 1-X_OFFSET //meters
-#define Y_MIN Y_OFFSET //meters
-#define Y_MAX 1-Y_OFFSET //meters
+
+#define DUTY_CYCLE_CONVERSION 1024      // Accepted duty cycle values are 0-1024
+#define PWM_FREQ_HZ 10000               //
+#define ROLLOVER_ANGLE_DEGS 180         //
+#define PULLEY_RADIUS 0.035             //meters
+#define MAX_PWM 20
+
+//Coordinate Definitions
+#define ICE_WIDTH 0.99                  //meters - total width of the playing surface
+#define ICE_HEIGHT 1.99                 //meters - total height of the playing surface
+#define X_OFFSET 0.0699                 //meters - 
+#define Y_OFFSET 0.0508                 //meters - 
+#define X_MIN X_OFFSET                  //meters - minimum x coordinate which mallet can go to
+#define X_MAX ICE_WIDTH-X_OFFSET        //meters - maximum x coordinate which mallet can go to
+#define Y_MIN Y_OFFSET                  //meters - minimum y coordinate which mallet can go to
+#define Y_MAX (ICE_HEIGHT/2)-Y_OFFSET   //meters - maximum y coordinate which mallet can go to
+
+//Agent Constants
+#define INTERSECTION_Y 0.2              //meters - y coordinate to intersect an approaching puck at
+#define HOME_Y 0.1                      //meters - y coordinate to return to and wait for next approaching shot
+#define STATIONARY_THRESHOLD 0.02       //meters - if puck has travelled less than this distance between frames it is stationary
+#define FRAMERATE 60                    //FPS - Of the camera
+#define DEFAULT_INTERCEPT_TIME 0.2      //s - Time to destination if destination is stationary
+#define DEFAULT_INTERCEPT_SPEED 4       //m/s - Speed to hit puck at
+
+//PID Controller Constants
+#define KP 1.0
+#define KI 0.0
+#define KD 0.0
 
 // Define Global Variables
 int left_revolutions = 0;
@@ -42,19 +52,11 @@ float previous_right_angle = 0;
 float left_offset = 0;
 float right_offset = 0;
 float start_time;
-float circle_radius = 0.15; //Meters
-float circle_period = 1; //Seconds
-float angular_frequency = 2 * PI / circle_period;
 float left_accumulated_error = 0;
 float right_accumulated_error = 0;
 float left_previous_error = 0;
 float right_previous_error = 0;
 double previous_time;
-float kp = 1.0;
-float ki = 0;
-// float kd = 0.0002;
-float kd = 0.0;
-float max_pwm = 20;
 
 std::array<float,2> prev_pos = {{0,0}};
 float t;
@@ -69,22 +71,160 @@ float vyf;
 float xf_prev, yf_prev;
 float x_puck, y_puck;
 
-// OLD FEEDFORWARD PARAMETERS
-// float ff[2][2][4] ={{{3.790419e-06,6.066717e-03,6.925599e-02,2.323943e-18},
-// {-1.501957e-06,-2.396739e-03, 6.811798e-17,-6.262513e-18}}, 
-// { {-1.501957e-06,-2.396739e-03, 8.406599e-17, 2.020639e-18},
-// {3.790419e-06, 6.052580e-03, 4.669578e-02,-4.398398e-17}}};
-
-float ff[2][2][4] = {{{4.474910e-06, 7.149068e-03, 5.342087e-02,-9.214972e-17,},
-{-1.964001e-06,-3.134045e-03, 7.759782e-17, 9.697753e-17,} }, 
-{ {-1.964001e-06,-3.134045e-03, 4.969833e-17, 2.967669e-18,},
-{4.474910e-06,7.149377e-03,5.391504e-02,6.167752e-17,} } };
-
+float ff[2][2][4] = {
+                        {
+                            {4.474910e-06, 7.149068e-03, 5.342087e-02,-9.214972e-17,},
+                            {-1.964001e-06,-3.134045e-03, 7.759782e-17, 9.697753e-17,} 
+                        }, 
+                        { 
+                            {-1.964001e-06,-3.134045e-03, 4.969833e-17, 2.967669e-18,},
+                            {4.474910e-06,7.149377e-03,5.391504e-02,6.167752e-17,} 
+                        } 
+                    };
 
 std::array<float,4> cx = {{0,0,0,0}};
 std::array<float,4> cy = {{0,0,0,0}};
 
-bool DISABLE_MOTORS = true;
+//Start Agent Code
+enum State{
+  DEFEND,
+  ATTACK,
+  HOME
+};
+
+enum Direction{
+  APPROACHING,
+  STATIONARY,
+  LEAVING
+};
+
+float location_of_intersection(float x1, float y1, float x2, float y2) {
+  float h = y1 - INTERSECTION_Y;
+  float w = h*(x1-x2)/(y1-y2) - x1;
+  if(int(floor(w/ICE_WIDTH))%2 == 0) {
+    return fmod(w, ICE_WIDTH);
+  }
+  else {
+    return ICE_WIDTH - fmod(w, ICE_WIDTH);
+  }
+}
+
+float time_to_intersection(float x1, float y1, float x2, float y2){
+  float h = y1 - INTERSECTION_Y;
+  float w = h*(x1-x2)/(y1-y2) - x1;
+  float position_delta = sqrtf((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1));
+  float speed = position_delta*FRAMERATE;
+  return sqrt(h*h + w*w)/speed;
+}
+
+State agent_state_selector(float x1, float y1, float x2, float y2){
+    bool puck_in_our_half = y2<ICE_HEIGHT/2;
+    Direction puck_direction = LEAVING;
+    if(abs(y2-y1) <STATIONARY_THRESHOLD and abs(x2-x1) <STATIONARY_THRESHOLD){
+        puck_direction = STATIONARY;
+    }
+    else if(y1-y2 > 0){
+        puck_direction = APPROACHING;
+    }
+    if(puck_direction == APPROACHING){
+        return DEFEND;
+    }
+    else if(puck_direction == STATIONARY and puck_in_our_half){
+        return ATTACK;
+    }
+    else{
+        return HOME;
+    }
+}
+
+float bounce_coordinate_calculator(bool left_bounce, float xi, float yi){
+    if(left_bounce){
+      return (2*xi*ICE_HEIGHT + ICE_WIDTH*yi)/(ICE_WIDTH + 2*xi);
+    }
+    else{
+      return (2*(ICE_WIDTH-xi)*ICE_HEIGHT + ICE_WIDTH*yi)/(ICE_WIDTH + 2*(ICE_WIDTH-xi));
+    }
+}
+
+float get_attack_angle(float x1, float y1, float x2, float y2){
+  return atan((y1-y2)/(x1-x2));
+}
+
+void execute_shot(float t, float x, float y, float theta, float v){
+  generate_path(x, y, theta, v, t)
+}
+
+void classical_agent(float xm, float ym, float x1, float y1, float x2, float y2){
+  State new_state = agent_state_selector(x1, y1, x2, y2);
+
+  float time_to_destination = DEFAULT_INTERCEPT_TIME;
+  float destination_x = 0;
+  float destination_y = 0;
+  float approach_angle = 0;
+  float approach_speed = DEFAULT_INTERCEPT_SPEED;
+
+  int shot_type = 0;
+  bool left_bounce = false;
+  bool straight_shot = false;
+  float x_bounce = 0;
+  float y_bounce = 0;
+  float intersection_x = 0;
+
+  switch(new_state){
+    case HOME:
+      destination_x = ICE_WIDTH/2;
+      destination_y = HOME_Y;
+      approach_angle = atan((ym-HOME_Y)/(xm-ICE_WIDTH/2));
+      approach_speed = 0;
+    break;
+    case ATTACK:
+      shot_type = random(0,3); //0 is left, 1 is right, 2 is straight
+      left_bounce = shot_type == 0;
+      straight_shot = shot_type == 2;
+      if(straight_shot){
+        approach_angle = get_attack_angle(x2, y2, ICE_WIDTH/2, ICE_HEIGHT);
+        destination_x = x2;
+        destination_y = y2;
+      }
+      else {
+        y_bounce = bounce_coordinate_calculator(left_bounce, x2, y2);
+        x_bounce = 0;
+        if(!left_bounce){
+          x_bounce = ICE_WIDTH;
+        }
+        approach_angle = get_attack_angle(x2, y2, x_bounce, y_bounce);
+        destination_x = x2;
+        destination_y = y2;
+      }
+    break;
+    case DEFEND:
+      shot_type = random(0,3); //0 is left, 1 is right, 2 is straight
+      left_bounce = shot_type == 0;
+      straight_shot = shot_type == 2;
+      intersection_x = location_of_intersection(x1, y1, x2, y2);
+      if(straight_shot){
+        approach_angle = get_attack_angle(intersection_x, INTERSECTION_Y, ICE_WIDTH/2, ICE_HEIGHT);
+        destination_x = intersection_x;
+        destination_y = INTERSECTION_Y;
+        time_to_destination = time_to_intersection(x1, y1, x2, y2);
+      }
+      else {
+        y_bounce = bounce_coordinate_calculator(left_bounce, x2, y2);
+        x_bounce = 0;
+        if(!left_bounce){
+          x_bounce = ICE_WIDTH;
+        }
+        approach_angle = get_attack_angle(intersection_x, INTERSECTION_Y, x_bounce, y_bounce);
+        destination_x = intersection_x;
+        destination_y = INTERSECTION_Y;
+        time_to_destination = time_to_intersection(x1, y1, x2, y2);
+      }
+    break;
+    execute_shot(time_to_destination, destination_x, destination_y, approach_angle, approach_speed);
+  }
+}
+//End Agent Code
+
 
 /*
 Read left and right motor angles from the encoders.
@@ -182,7 +322,7 @@ Return the sum of the PID terms
 float pid(float error, float accumulated_error, float previous_error, double dt) {
     float error_derivative = (error - previous_error) / dt;
 
-    return error * kp + accumulated_error * ki + error_derivative * kd;
+    return error * KP + accumulated_error * KI + error_derivative * KD;
 }
 
 /*
@@ -340,7 +480,7 @@ Generate the path and its coefficients for the given HLC target. Calculations fr
 https://en.wikipedia.org/wiki/B%C3%A9zier_curve
 */
 
-void generate_path(float x_puck, float y_puck, float vf_magnitude, float path_time) {
+void generate_path(float x_puck, float y_puck, float vf_theta, float vf_magnitude, float path_time) {
     float table_length = 2;
     float x_goal = 0.52;
     float mallet_plus_puck_radius = 0.05 + 0.03175; 
@@ -363,8 +503,8 @@ void generate_path(float x_puck, float y_puck, float vf_magnitude, float path_ti
         vx_initial = 0;
         vy_initial = 0;
     }
-    float vf_x = x_goal - x_puck;
-    float vf_y = table_length - y_puck;
+    float vf_x = math.cos(theta);
+    float vf_y = math.sin(theta);
 
     float original_vf_norm = sqrt(vf_x*vf_x + vf_y*vf_y);
 
@@ -415,6 +555,16 @@ void get_target_from_hlc() {
     tf = t + traj_durations[path_section_num];
 }
 
+void read_camera(){
+    if (Serial2.available()) {
+        temp = read_float(',');
+        x_puck = read_float(',');
+        y_puck = read_float(',');
+        temp = read_float(',');
+        temp = read_float('\n');
+    }
+}
+
 HardwareSerial Serial2(PA3, PA2);
 void setup() {
     Serial2.begin(460800);
@@ -460,6 +610,18 @@ void setup() {
 
 void loop() {
     t = (micros() - start_time) / 1000000.0;
+
+    std::array<float,2> current_angles = read_motor_angles();
+    std::array<float,2> current_pos = theta_to_xy(current_angles[0], current_angles[1]);
+
+    xm = current_pos[0];
+    ym = current_pos[1];
+    x1 = x2;
+    y1 = y2;
+    x2 = x_puck;
+    y2 = y_puck;
+
+    classical_agent(xm, ym, x1, y1, x2, y2);
 
     if (t > tf) {
         path_start_time = t;
