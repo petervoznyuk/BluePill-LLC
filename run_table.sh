9e64ac3455b944944c95a1a116915f356ac3f140
George9154 <george.hy.xu@gmail.com>
2024-04-06 18:24:49 -0700

    removed target monitor

--- run_table.sh
+++ run_table.sh
@@ -4,4 +4,4 @@
 # # See https://docs.platformio.org/en/latest/core/userguide/device/cmd_monitor.html#cmd-device-monitor-filters
 # ~/.platformio/penv/Scripts/platformio.exe device monitor --port COM5 --baud 460800 --eol=LF --filter send_on_enter > data/2024-03-06/$(date +%Y%m%d)_time$(date +%H%M).csv
 
-~/.platformio/penv/Scripts/platformio.exe run --target upload --target monitor --environment bluepill_f103c8 
+~/.platformio/penv/Scripts/platformio.exe run --target upload --environment bluepill_f103c8 
